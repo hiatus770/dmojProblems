d98c5d6257c8e2473c570ae7a2979cadba47e80f
hiatus770-chromebook <hiatus.cool@gmail.com>
2022-03-12 16:32:56 -0500

    fard

--- Solutions/DMOPC FEB/P3/P3.cpp
+++ Solutions/DMOPC FEB/P3/P3.cpp
@@ -2,25 +2,72 @@
 using namespace std;
 const int MD = 1001; 
 #define inputJunk ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define max 1501
+#define ell "\n"
+#define f first
+#define s second
+#define pb push_back
+#define forl(n) for (int i = 0; i < n; i++)
+typedef long long ll; 
+#define MAX 1500
 
 int n, m, k; 
 
-int grid[max+1][max+1]; 
 
+int grid[MAX+1][MAX+1]; 
+int dist[MAX+1][MAX+1]; 
+pair<int, pair<int, int>> c[MAX+1]; 
+queue<pair<int, int>> q; 
+
+void bfs(){
+    for (int i = 0; i < z; i++){
+        q.push({c[i].f.f, c[i].f.s}); 
+    }
+    pair<int, int> cur; 
+    while(!q.empty()){
+        cur = q.front(); 
+        q.pop(); 
+
+        if (c.f+1 <= m && grid[c.f+1][c.s] == 0  && dist[c.f][c.s] < k){
+            grid[c.f+1][c.s] = grid[c.f][c.s]; 
+            q.push({c.f+1, c.s}); 
+            dist[c.f+1][c.s] = dist[c.f][c.s];
+        }
+       if (c.f-1 <= m && grid[c.f-1][c.s] == 0  && dist[c.f][c.s] < k){
+            grid[c.f-1][c.s] = grid[c.f][c.s]; 
+            q.push({c.f-1, c.s}); 
+            dist[c.f-1][c.s] = dist[c.f][c.s];
+        }
+        if (c.f+1 <= m && grid[c.f+1][c.s] == 0  && dist[c.f][c.s] < k){
+            grid[c.f+1][c.s] = grid[c.f][c.s]; 
+            q.push({c.f+1, c.s}); 
+            dist[c.f+1][c.s] = dist[c.f][c.s];
+        }
+    }
+}
 
 int main(){
     inputJunk 
-    cin >> n >> m >> k; 
+    cin >> n >> m >> k;  
     int z = 0; 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             cin >> grid[j][i]; 
             if (grid[j][i] != 0){
-                
-            } 
+                pair<int, pair<int, int>> thing = {grid[j][i], {i, j}}; 
+                c[z++] = thing;
+                dist[j][i] == 0;  
+            }
         }
     }
+
+    bfs();
+
+    sort(c, c+(n*m), greater<pair<int, pair<int, int>>>()); 
+    
+    for (int i = 0; i < z; i++){
+        cout << c[i].f << endl; 
+    }
+
 
 
 }
--- Solutions/ccc06s1/Maternity.py
+++ Solutions/ccc06s1/Maternity.py
@@ -2,10 +2,11 @@ parent1 = input()
 parent2 = input()
 # n = int(input())
 
-# l and u 
+# lower or upper allowed with o, l, and u
 l = []
 
-for i in range(0, len(parent1), 2):
-    print(parent1[i], parent1[i+1])
-    print(parent2[i], parent2[i+1])
+for i in range(1, len(parent1), 2):
+    print(parent1[i-1], parent1[i])
+    print(parent2[i-1], parent2[i])
+    
     
