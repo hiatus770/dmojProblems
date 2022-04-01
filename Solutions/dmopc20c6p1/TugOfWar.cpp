3e98b3601cd3813ab1f7ac1ab8e57e5805e1adac
hiatus770-chromebook <hiatus.cool@gmail.com>
2022-04-01 15:06:23 -0400

    newest

---
+++ Solutions/dmopc20c6p1/TugOfWar.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define inputJunk ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ell "\n";
+#define f first
+#define s second
+#define pb push_back
+#define forl(n) for (int i = 0; i < n; i++)
+typedef long long ll; 
+typedef pair<int, int> pInt; 
+#define mxn 1000
+
+int psa[mxn];
+
+int main(){
+    /* Tug of war? More like tug on deez nuts */ 
+
+}
