d8a8e48db7bd0975806b41f6e82e9e90441f0db4
Hiatus <77402029+hiatus770@users.noreply.github.com>
2022-04-14 17:52:06 -0400

    Update Solution.cpp

--- Solutions/CakeProblem/Solution.cpp
+++ Solutions/CakeProblem/Solution.cpp
@@ -9,26 +9,31 @@ const int mxN = 5005;
 int n,m,k,q,arr[mxN][mxN],diff[mxN][mxN];
 ll pre[mxN][mxN];
 int main(){
+    // input 
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> m >> k;
     for(int i = 0; i < k; i++){
         int a,b,c,d;
         cin >> a >> b >> c >> d;
+        // Difference array setting stuff   
         for(int j = a; j <= c; j++){
             diff[j][b]++;
             diff[j][d+1]--;
         }
     }
+    // running through difference array to make into real values
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
             arr[i][j] = arr[i][j-1] + diff[i][j];
         }
     }
+    // Generating 2d psa 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= m; j++){
             pre[i][j] = arr[i][j] + pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
         }
     }
+    // utilizing 2d psa 
     cin >> q;
     for(int i = 0; i < q; i++){
         int a,b,c,d;
