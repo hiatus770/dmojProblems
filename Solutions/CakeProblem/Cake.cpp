831f01cb931734aabd9f2580241ebbeda4082e9a
Hiatus <77402029+hiatus770@users.noreply.github.com>
2022-04-14 17:51:46 -0400

    Update Cake.cpp

--- Solutions/CakeProblem/Cake.cpp
+++ Solutions/CakeProblem/Cake.cpp
@@ -20,6 +20,7 @@ int main(){
     forl(k){
         int a, b, c, d; 
         cin >> a >> b >> c >> d; 
+        //    (x1, y1)  (x2, y2) 
         for (int r = a; r <= c; r++){
             arr[r][b]++; 
             arr[r][d+1]--; 
@@ -27,8 +28,45 @@ int main(){
     }
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
+            arr[j][i] = arr[j][i-1] + arr[j][i];  
+        }
+
+    }
+
+    cout << "ORIGINAL:\n"; 
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
             cout << arr[j][i] << " ";
         }
         cout << endl; 
     }
+
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            pre[j][i] = arr[j][i] + pre[j-1][i] + pre[j][i-1] - pre[j-1][i-1];  
+        }
+    }
+
+    cout << "PSA:\n"; 
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            cout << pre[j][i] << " ";
+        }
+        cout << endl; 
+    }
+
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            pre[j][i] = arr[j][i] + pre[j-1][i] + pre[j][i-1]; 
+        }
+    }
+
+    cin >> q; 
+    forl(q){
+        int a, b, c, d; 
+        cin >> a >> b >> c >> d;
+        cout << pre[d][c]-pre[d][a-1]-pre[b-1][c]+pre[b-1][a-1] << ell 
+        cout << pre[d][c] << "-" << pre[d][a-1] << "-" << pre[b-1][c] << "+" << pre[b-1][a-1] << ell
+    }
+/* (a,b) to (c, d) is the rectangle that we want */ 
 }
