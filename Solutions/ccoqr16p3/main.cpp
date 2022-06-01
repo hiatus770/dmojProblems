b5e5820b742e59ec7cc25b6b543bfc41f9d75bc7
hiatus770 <hiatus.cool@gmail.com>
2022-06-01 19:26:21 -0400

    aldkfj

---
+++ Solutions/ccoqr16p3/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define inputJunk ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ell "\n";
+#define f first
+#define s second
+#define pb push_back
+#define forl(n) for (int i = 0; i < n; i++)
+typedef long long ll; 
+typedef pair<int, int> pInt; 
+#define mxn 500005
+
+// Data structure problem on dmoj 
+int n; 
+pair<int, int> arr[mxn]; 
+
+// returns the sum of a series of number from a to b
+int sum(int a, int b){
+    int sum = 0; 
+    
+}
+
+int main(){
+    cin >> n;
+    forl(n){
+        cin >> arr[i].f >> arr[i].s;
+    }
+
+}
